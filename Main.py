8655bcba348e48a5657645063e4a19c9d014494f
Fynn <81396054+FynnMallon@users.noreply.github.com>
2022-05-11 23:25:00 +1000

    Established Game Plan
    Planned AI and found necessary libraries

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Alpacca to feed historical data 
+# Set Dataset initially to 
+
+# Options for AI to buy sell hold @ strike price
+# If/When strike price reached order placed
+# AI generational progression based on Profit/Loss
+
+# Use Tensor / Machine Learning
+# Unsupervised Learning
+# Ai predicts if next dataset will be up or down
+
+# For linear algebra
+import numpy as np
+# For data processing
+import pandas as pd
